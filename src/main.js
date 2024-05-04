da65481db79ae14d4e48068f8f8a6a6b61eb2544
Hiansdt <vinijao123690@gmail.com>
2024-05-04 10:51:30 -0300

    FEAT(Global): Create Base Files [Refs: #1]

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import AreaManager from './managers/areaManager.js';
+import GameManager from './managers/gameManager.js';
+import { GAME_SETTINGS } from './utils/constants.js';
+import Player from './models/player.js';
+
+const player = new Player(
+    GAME_SETTINGS.STARTING_PLAYER_X,
+    GAME_SETTINGS.STARTING_PLAYER_Y,
+    GAME_SETTINGS.PLAYER_SPEED,
+    GAME_SETTINGS.PLAYER_WIDTH,
+    GAME_SETTINGS.PLAYER_HEIGHT,
+    GAME_SETTINGS.PLAYER_COLOR
+);
+const areaManager = new AreaManager(player);
+const gameManager = new GameManager(player, areaManager);
+
+gameManager.startGameLoop();
+areaManager.createInitialArea(10, 3, { x: 0, y: 0 });
