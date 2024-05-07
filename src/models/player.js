3035ca5b53b65ab5aa190052245211c26fe0eff9
Hiansdt <vinijao123690@gmail.com>
2024-05-07 11:07:34 -0300

    FIX(Tree Collision): Adjust tree collision boundaries [Refs: #19]

--- src/models/player.js
+++ src/models/player.js
@@ -67,41 +67,37 @@ export default class Player {
     }
 
     checkTreeCollision(newXPercent, newYPercent, trees) {
-        // Implement tree collision logic based on player position and tree positions
         const gameContainer = document.querySelector('#game-container');
         const gameContainerRect = gameContainer.getBoundingClientRect();
         const gameContainerWidth = gameContainerRect.width;
         const gameContainerHeight = gameContainerRect.height;
     
-        // Convert percentages to pixel values based on game container dimensions
         const newX = (newXPercent / 100) * gameContainerWidth;
         const newY = (newYPercent / 100) * gameContainerHeight;
         for (let tree of trees) {
             const treeRect = tree.element.getBoundingClientRect();
 
-            // Calculate the potential new player rectangle based on newX, newY
             const playerRect = {
-                top: newY,
-                bottom: newY + this.height,
-                left: newX,
-                right: newX + this.width
+                top: newY - 10,
+                bottom: newY + this.height - 10,
+                left: newX - 9,
+                right: newX + this.width - 20
             };
 
-            // Check for collision between player and tree rectangles
             if (
                 playerRect.left < treeRect.right &&
                 playerRect.right > treeRect.left &&
                 playerRect.top < treeRect.bottom &&
                 playerRect.bottom > treeRect.top
             ) {
-                // Collision detected, return true
+                console.log(treeRect)
+                console.log(playerRect)
                 return true;
             }
         }
     }
 
     checkCoinCollision(coins) {
-        // Implement coin collision logic based on player position and coin positions
     }
 
     update(deltaTime, trees, coins) {
