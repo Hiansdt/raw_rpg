fb38535546cae5b6c7de075975f3490b2a98c84d
Hiansdt <vinijao123690@gmail.com>
2024-05-07 17:13:36 -0300

    FIX(Trees): Create function to get tree position and add valitation to prevent trees from spawning near the game's border limit [Refs: #22]

--- src/models/tree.js
+++ src/models/tree.js
@@ -2,11 +2,10 @@ import { GAME_SETTINGS } from "../utils/constants.js";
 export class Tree {
     constructor() {
         this.id = Math.floor(Math.random() * 1000);
-        this.x = Math.floor(Math.random() * 100);
-        this.y = Math.floor(Math.random() * 100);
         this.width = GAME_SETTINGS.TREE_WIDTH;
         this.height = GAME_SETTINGS.TREE_HEIGHT;
         this.element = null;
+        this.getTreePosition();
     }
 
     createTreeElement() {
@@ -23,5 +22,15 @@ export class Tree {
         treeElement.style.backgroundSize = 'cover';
         this.element = treeElement;
         return treeElement;
+    }
+
+    getTreePosition() {
+        const x = Math.random() * 100;
+        const y = Math.random() * 100;
+        if (x < 5 || x > 95 || y < 5 || y > 80) {
+            return this.getTreePosition();
+        }
+        this.x = x;
+        this.y = y;
     }
 }
